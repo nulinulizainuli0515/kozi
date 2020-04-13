6ce7a96fc4e0faf1b27d0d3a11860708e3b0f62e
nulinulizainuli0515 <61219184+nulinulizainuli0515@users.noreply.github.com>
2020-04-13 23:34:39 +0900

    iiii

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_012655) do
+ActiveRecord::Schema.define(version: 2020_04_13_135930) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
